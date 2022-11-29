fa44fd7bd332b4784a9205e77b85e0dd27ae54ce
byunm <byunm@DESKTOP-PUKB97F>
2022-11-29 15:23:38 +0900

    221129 - minjae - 커밋 테스트

--- aweek/src/main/java/chat/controller/ChatController.java
+++ aweek/src/main/java/chat/controller/ChatController.java
@@ -1,8 +1,65 @@
 package chat.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import chat.service.face.ChatService;
 
 @Controller
+@RequestMapping("/chat")
 public class ChatController {
 
+	//로그객체
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private ChatService chatService;
+	
+	@GetMapping("/login")
+	public void chatLogin() {
+		logger.info("/chat/login");
+	}
+	
+	@PostMapping("/login")
+	public String loginProc(String id, HttpSession session) {
+		logger.info("/chat/loginProc");
+		
+		session.setAttribute("id", id);
+		
+		return "redirect:/chat/main";
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
