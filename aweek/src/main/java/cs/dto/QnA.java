840b0d2bfe5f1f251073a8ff240eed4a764bad4c
daun0817 <daun0817@DESKTOP-87SLJ18>
2022-11-29 22:02:39 +0900

    221129 - daun - 로고 jpg->png, header조금

--- aweek/src/main/java/cs/dto/QnA.java
+++ aweek/src/main/java/cs/dto/QnA.java
@@ -1,5 +1,80 @@
 package cs.dto;
 
+import java.util.Date;
+
 public class QnA {
 
+	private int qnaNo;
+	private int fileNo;
+	private int adminNo;
+	private String qnaTitle;
+	private String qnaContent;
+	private Date qnaDate;
+	
+	public QnA() {}
+
+	public QnA(int qnaNo, int fileNo, int adminNo, String qnaTitle, String qnaContent, Date qnaDate) {
+		super();
+		this.qnaNo = qnaNo;
+		this.fileNo = fileNo;
+		this.adminNo = adminNo;
+		this.qnaTitle = qnaTitle;
+		this.qnaContent = qnaContent;
+		this.qnaDate = qnaDate;
+	}
+
+	@Override
+	public String toString() {
+		return "QnA [qnaNo=" + qnaNo + ", fileNo=" + fileNo + ", adminNo=" + adminNo + ", qnaTitle=" + qnaTitle
+				+ ", qnaContent=" + qnaContent + ", qnaDate=" + qnaDate + "]";
+	}
+
+	public int getQnaNo() {
+		return qnaNo;
+	}
+
+	public void setQnaNo(int qnaNo) {
+		this.qnaNo = qnaNo;
+	}
+
+	public int getFileNo() {
+		return fileNo;
+	}
+
+	public void setFileNo(int fileNo) {
+		this.fileNo = fileNo;
+	}
+
+	public int getAdminNo() {
+		return adminNo;
+	}
+
+	public void setAdminNo(int adminNo) {
+		this.adminNo = adminNo;
+	}
+
+	public String getQnaTitle() {
+		return qnaTitle;
+	}
+
+	public void setQnaTitle(String qnaTitle) {
+		this.qnaTitle = qnaTitle;
+	}
+
+	public String getQnaContent() {
+		return qnaContent;
+	}
+
+	public void setQnaContent(String qnaContent) {
+		this.qnaContent = qnaContent;
+	}
+
+	public Date getQnaDate() {
+		return qnaDate;
+	}
+
+	public void setQnaDate(Date qnaDate) {
+		this.qnaDate = qnaDate;
+	}
+	
 }
--- aweek/src/main/java/room/dto/Room.java
+++ aweek/src/main/java/room/dto/Room.java
@@ -2,4 +2,99 @@ package room.dto;
 
 public class Room {
 
+	private int roomNo;
+	private int userNo;
+	private int gcalNo;
+	private int roomCategoryNo;
+	private String roomName;
+	private String introduce;
+	private int roomMember;
+	private int roomPublic;
+	
+	public Room() {	}
+
+	public Room(int roomNo, int userNo, int gcalNo, int roomCategoryNo, String roomName, String introduce,
+			int roomMember, int roomPublic) {
+		super();
+		this.roomNo = roomNo;
+		this.userNo = userNo;
+		this.gcalNo = gcalNo;
+		this.roomCategoryNo = roomCategoryNo;
+		this.roomName = roomName;
+		this.introduce = introduce;
+		this.roomMember = roomMember;
+		this.roomPublic = roomPublic;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [roomNo=" + roomNo + ", userNo=" + userNo + ", gcalNo=" + gcalNo + ", roomCategoryNo="
+				+ roomCategoryNo + ", roomName=" + roomName + ", introduce=" + introduce + ", roomMember=" + roomMember
+				+ ", roomPublic=" + roomPublic + "]";
+	}
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public int getGcalNo() {
+		return gcalNo;
+	}
+
+	public void setGcalNo(int gcalNo) {
+		this.gcalNo = gcalNo;
+	}
+
+	public int getRoomCategoryNo() {
+		return roomCategoryNo;
+	}
+
+	public void setRoomCategoryNo(int roomCategoryNo) {
+		this.roomCategoryNo = roomCategoryNo;
+	}
+
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+
+	public String getIntroduce() {
+		return introduce;
+	}
+
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+
+	public int getRoomMember() {
+		return roomMember;
+	}
+
+	public void setRoomMember(int roomMember) {
+		this.roomMember = roomMember;
+	}
+
+	public int getRoomPublic() {
+		return roomPublic;
+	}
+
+	public void setRoomPublic(int roomPublic) {
+		this.roomPublic = roomPublic;
+	}
+
 }
