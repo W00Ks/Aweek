086ce77b69f9bdc274dab4a0b7696b25aa4b3599
daun0817 <daun0817@DESKTOP-87SLJ18>
2022-11-30 18:09:14 +0900

    221130 - daun - roomHeader.jsp

--- aweek/src/main/webapp/resources/js/roomMain.js
+++ aweek/src/main/webapp/resources/js/roomMain.js
@@ -1,4 +1,9 @@
 /**
  * 
  */
- 
+const toggleBtn = document.querySelector('.header__toggleBtn');
+const menu = document.querySelector('.header__menu');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+})
